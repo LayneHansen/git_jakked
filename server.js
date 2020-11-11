369b32a2b63655290a275ce10c66809bed24276f
LayneHansen <layne.d.hansen@gmail.com>
2020-11-11 15:18:52 -0700

    set up server, connected index.html

---
+++ server.js
@@ -0,0 +1,62 @@
+var express = require("express");
+
+var app = express();
+
+var logger = require("morgan");
+
+// app.use(express.static("public"));
+
+// // Parse the request as JSON
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(logger("dev"));
+
+var PORT = process.env.PORT || 3000;
+
+// app.get('/', (request, response) => {
+//     // params, query
+//     let demo = request.params.demo
+//     // response.send("okay")
+//     // response.json({ test: true })
+// })
+
+app.get('/', (request, response) => {
+    console.log(__dirname + '/index.html')
+    response.sendFile(__dirname + '/index.html')
+})
+
+// app.get('/', (request, response) => {
+//     console.log(__dirname + '/index.html')
+//     response.sendFile(__dirname + '/index.html')
+// })
+
+// app.get('/', (request, response) => {
+//     console.log(__dirname + '/index.html')
+//     response.sendFile(__dirname + '/index.html')
+// })
+
+
+
+
+app.post('/', (request, response) => {
+    let test = request.body.test
+    response.send("post working")
+})
+
+
+
+
+// // Set handlebars, declaring "main" and default
+// var exphbs = require("express-handlebars");
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+// // Import routes and give server access
+// var router = require("./controllers/burgers_controller.js");
+
+// app.use(router);
+
+app.listen(PORT, function () {
+    console.log("App now listening at localhost:" + PORT);
+});
